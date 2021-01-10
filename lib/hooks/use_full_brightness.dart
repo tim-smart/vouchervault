760693034c572321f0127f459746aade515a4a0d
Tim Smart <tim.smart@arisechurch.com>
2021-01-11 11:31:03 +1300

    Updates for ios build

--- lib/hooks/use_full_brightness.dart
+++ lib/hooks/use_full_brightness.dart
@@ -3,7 +3,6 @@ import 'package:screen/screen.dart';
 
 void useFullBrightness({bool enabled = true}) => useEffect(() {
       if (!enabled) return () {};
-
       final future = Screen.setBrightness(1);
       return () => future.then((_) => Screen.setBrightness(-1));
     });
