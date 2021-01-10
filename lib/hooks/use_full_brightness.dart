b3bc720be537bdcc53f33994d0a6529e3964c67b
Tim Smart <tim.smart@arisechurch.com>
2021-01-11 12:06:41 +1300

    Tweak ios useFullBrightness

--- lib/hooks/use_full_brightness.dart
+++ lib/hooks/use_full_brightness.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:screen/screen.dart';
 
+Future<double> _getBrightness() =>
+    Platform.isAndroid ? Future.value(-1) : Screen.brightness;
+
 void useFullBrightness({bool enabled = true}) => useEffect(() {
       if (!enabled) return () {};
-      final future = Screen.setBrightness(1);
-      return () => future.then((_) => Screen.setBrightness(-1));
+
+      final future = _getBrightness().then((b) {
+        Screen.setBrightness(1);
+        return b;
+      });
+      return () => future.then((b) => Screen.setBrightness(b));
     });
