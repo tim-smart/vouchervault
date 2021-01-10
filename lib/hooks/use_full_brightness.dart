c7ffa18530b31c4a657abfe97e294711aa0bb1d7
Tim Smart <tim.smart@arisechurch.com>
2021-01-11 11:03:03 +1300

    Reset brightness back to -1 after using full brightness

--- lib/hooks/use_full_brightness.dart
+++ lib/hooks/use_full_brightness.dart
@@ -1,14 +1,9 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:screen/screen.dart';
 
 void useFullBrightness({bool enabled = true}) => useEffect(() {
       if (!enabled) return () {};
 
-      var initialBrightness = none<double>();
-      Screen.brightness.then((b) {
-        initialBrightness = some(b);
-        Screen.setBrightness(1);
-      });
-      return () => initialBrightness.map(Screen.setBrightness);
+      final future = Screen.setBrightness(1);
+      return () => future.then((_) => Screen.setBrightness(-1));
     });
