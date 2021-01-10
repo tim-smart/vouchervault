fc4656f9a979b469d53529cf10b5d067a2a28d61
Tim Smart <tim.smart@arisechurch.com>
2021-01-11 11:30:50 +1300

    Remove unused kotlin code

--- android/app/src/main/kotlin/co/timsmart/vouchervault/MainActivity.kt
+++ android/app/src/main/kotlin/co/timsmart/vouchervault/MainActivity.kt
@@ -1,13 +1,5 @@
 package co.timsmart.vouchervault
 
-import android.os.Bundle
-import android.view.View
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.statusBarColor = 0x00000000;
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-    }
-}
+class MainActivity : FlutterActivity() {}
