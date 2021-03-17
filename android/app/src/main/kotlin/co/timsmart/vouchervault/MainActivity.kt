3709ff03763b549341421611c5bbd4fe5c9805f4
Tim Smart <tim.smart@arisechurch.com>
2021-03-18 11:35:35 +1300

    Fix shared preferences

--- android/app/src/main/kotlin/co/timsmart/vouchervault/MainActivity.kt
+++ android/app/src/main/kotlin/co/timsmart/vouchervault/MainActivity.kt
@@ -3,11 +3,17 @@ package co.timsmart.vouchervault
 import android.os.Bundle
 import android.view.View
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = 0x00000000;
+        window.statusBarColor = 0x00000000
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
