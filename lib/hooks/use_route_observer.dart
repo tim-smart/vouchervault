67116f63f305651e51c7244b5d57a76d07ed0c78
Tim Smart <tim.smart@arisechurch.com>
2021-03-27 14:00:11 +1300

    Refactor route observer hoook

--- lib/hooks/use_route_observer.dart
+++ lib/hooks/use_route_observer.dart
@@ -1,38 +1,62 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class _RouteCallbacks with RouteAware {
-  const _RouteCallbacks({
-    this.handleDidPopNext = const None(),
-    this.handleDidPush = const None(),
-    this.handleDidPop = const None(),
-    this.handleDidPushNext = const None(),
-  });
+class _RouteObserverHook extends Hook<void> {
+  const _RouteObserverHook(
+    this.routeObserver, {
+    this.didPopNext = const None(),
+    this.didPush = const None(),
+    this.didPop = const None(),
+    this.didPushNext = const None(),
+    List<Object?> keys = const [],
+  }) : super(keys: keys);
 
-  final Option<VoidCallback> handleDidPopNext;
-  final Option<VoidCallback> handleDidPush;
-  final Option<VoidCallback> handleDidPop;
-  final Option<VoidCallback> handleDidPushNext;
+  final RouteObserver<ModalRoute> routeObserver;
+  final Option<VoidCallback> didPopNext;
+  final Option<VoidCallback> didPush;
+  final Option<VoidCallback> didPop;
+  final Option<VoidCallback> didPushNext;
 
   @override
+  _RouteObserverHookState createState() => _RouteObserverHookState();
+}
+
+class _RouteObserverHookState extends HookState<void, _RouteObserverHook>
+    implements RouteAware {
+  ModalRoute? _route;
+
+  @override
+  void build(BuildContext context) {
+    if (_route != null) return;
+
+    _route = ModalRoute.of(context);
+    if (_route != null) {
+      hook.routeObserver.subscribe(this, _route!);
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_route != null) {
+      hook.routeObserver.unsubscribe(this);
+    }
+  }
+
   void didPopNext() {
-    handleDidPopNext.map((f) => f());
+    hook.didPopNext.map((f) => f());
   }
 
-  @override
   void didPush() {
-    handleDidPush.map((f) => f());
+    hook.didPush.map((f) => f());
   }
 
-  @override
   void didPop() {
-    handleDidPop.map((f) => f());
+    hook.didPop.map((f) => f());
   }
 
-  @override
   void didPushNext() {
-    handleDidPushNext.map((f) => f());
+    hook.didPushNext.map((f) => f());
   }
 }
 
@@ -44,19 +68,12 @@ void useRouteObserver(
   Option<VoidCallback> didPushNext = const None(),
   List<Object?> keys = const [],
 }) {
-  final context = useContext();
-  final route = ModalRoute.of(context);
-
-  useEffect(() {
-    if (route == null) return () {};
-
-    final callbacks = _RouteCallbacks(
-      handleDidPop: didPop,
-      handleDidPopNext: didPopNext,
-      handleDidPush: didPush,
-      handleDidPushNext: didPushNext,
-    );
-    routeObserver.subscribe(callbacks, route);
-    return () => routeObserver.unsubscribe(callbacks);
-  }, [route, routeObserver, ...keys]);
+  use(_RouteObserverHook(
+    routeObserver,
+    didPop: didPop,
+    didPopNext: didPopNext,
+    didPush: didPush,
+    didPushNext: didPushNext,
+    keys: keys,
+  ));
 }
