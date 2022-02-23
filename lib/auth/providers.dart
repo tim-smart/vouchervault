c80a691b5b0f03c1f472af3842178cb8beceef4a
Tim Smart <tim.smart@arisechurch.com>
2022-02-23 14:26:48 +1300

    Add persistedSMProvider helper

--- lib/auth/providers.dart
+++ lib/auth/providers.dart
@@ -2,45 +2,25 @@ import 'package:fpdt/fpdt.dart';
 import 'package:fpdt/riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:local_auth/local_auth.dart';
-import 'package:riverpod_persistence/riverpod_persistence.dart';
-import 'package:vouchervault/app/providers.dart';
-import 'package:vouchervault/auth/ops.dart';
 import 'package:vouchervault/auth/model.dart';
+import 'package:vouchervault/auth/ops.dart';
 import 'package:vouchervault/lib/riverpod.dart';
 
 final localAuthProvider = Provider((ref) => LocalAuthentication());
 
-typedef AuthStateMachine = StateRTEMachine<AuthState, RefRead, String>;
-
-final authSMProvider = persistProvider<Provider<AuthStateMachine>, AuthState>(
-  (read, write) => Provider((ref) {
-    final sm = stateMachineProvider<AuthStateMachine>(
-      ref,
-      StateRTEMachine(
-        read(ref) ?? AuthState.notAvailable,
-        ref.read,
-      ),
-    );
-
-    ref.onDispose(sm.stream.listen(write(ref)).cancel);
-
-    // Init
-    sm.run(init);
-
-    return sm;
-  }),
-  buildStorage: (ref) => SharedPreferencesStorage(
-    // Use same key as previous AuthBloc
-    keyPrefix: 'pbs_',
-    key: 'AuthBloc',
-    toJson: (s) => s.toJson(),
-    fromJson: (json) => AuthState.fromJson(json),
-    instance: ref.watch(sharedPreferencesProvider),
-  ),
+final authSMProvider = persistedSMProvider<AuthState, RefRead, String>(
+  create: (ref, initial) => StateRTEMachine(
+    initial ?? AuthState.notAvailable,
+    ref.read,
+  )..run(init),
+  key: 'AuthBloc',
+  fromJson: (json) => AuthState.fromJson(json),
+  toJson: (s) => s.toJson(),
 );
 
 final authProvider = Provider(
-    (ref) => stateMachineStateProvider(ref, ref.watch(authSMProvider)));
+  (ref) => stateMachineStateProvider(ref, ref.watch(authSMProvider)),
+);
 
 final authEnabledProvider = Provider((ref) => ref.watch(authProvider).enabled);
 final authAvailableProvider =
