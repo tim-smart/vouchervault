6f116420dbb8bef23da4104008a1417923e80a0e
Tim Smart <tim.smart@arisechurch.com>
2022-02-24 13:49:15 +1300

    Update screenshots

--- tools/screenshots.dart
+++ tools/screenshots.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
   // Shutdown all the running emulators
   await emu.shutdownAll();
 
-  await emu.forEach(['Nexus_5X'])((device) async {
+  await emu.forEach(['Pixel_3'])((device) async {
     final p = await emu.drive(
       device,
       'test_driver/main.dart',
