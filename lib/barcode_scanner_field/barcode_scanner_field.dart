e6960755e47ced26079d292fe5f002156de16598
Tim Smart <tim.smart@arisechurch.com>
2022-05-21 19:42:42 +1200

    remove unused import

--- lib/barcode_scanner_field/barcode_scanner_field.dart
+++ lib/barcode_scanner_field/barcode_scanner_field.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:barcode_widget/barcode_widget.dart' show Barcode, BarcodeWidget;
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
