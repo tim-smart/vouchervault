b87a48291f8ccefa88ac42a64cb1cae633829420
Tim Smart <tim.smart@arisechurch.com>
2022-09-20 11:52:13 +1200

    update deps

--- lib/auth/atoms.dart
+++ lib/auth/atoms.dart
@@ -4,12 +4,12 @@ import 'package:local_auth/local_auth.dart';
 import 'package:vouchervault/auth/auth.dart';
 import 'package:vouchervault/lib/nucleus.dart';
 
-final localAuthProvider = atom((get) => LocalAuthentication());
+final localAuth = atom((get) => LocalAuthentication());
 
 final authState = smAtom<AuthState, AuthContext, String>(
   (get, initial) => StateRTEMachine(
     initial?.init() ?? AuthState.notAvailable,
-    AuthContext(localAuth: get(localAuthProvider)),
+    AuthContext(localAuth: get(localAuth)),
   )..run(init),
   key: 'pbs_AuthBloc',
   fromJson: (json) => AuthState.fromJson(json),
