ea7362c9d0656ee31f6c8e4e4225f2dea45d80ca
Tim Smart <hello@timsmart.co>
2023-02-18 22:15:21 +1300

    chore: update built files

--- lib/voucher_form/barcode_scanner/models/ml_error.freezed.dart
+++ lib/voucher_form/barcode_scanner/models/ml_error.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'ml_error.dart';
 
@@ -350,14 +350,14 @@ class __$$_MlErrorMlKitCopyWithImpl<$Res>
   @override
   $Res call({
     Object? op = null,
-    Object? err = null,
+    Object? err = freezed,
   }) {
     return _then(_$_MlErrorMlKit(
       op: null == op
           ? _value.op
           : op // ignore: cast_nullable_to_non_nullable
               as String,
-      err: null == err
+      err: freezed == err
           ? _value.err
           : err // ignore: cast_nullable_to_non_nullable
               as dynamic,
